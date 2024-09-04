a29db43f25bddb098ff0c6c3357d85d5d3b79005
Jafar Shaikh <jafar@datavoice.co.in>
2024-09-04 21:09:17 +0530

    spellcheck page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SpellCheckApp from './SpellCheckApp';
+
+function App() {
+  return (
+    <div className="App">
+    <SpellCheckApp />
+    </div>
+  );
+}
+
+export default App;
